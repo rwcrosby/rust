fb2d98fbd3ea31f42d6c4fd8bf870c45a641520c
Ralph Crosby <rwc@cs.tamu.edu>
2020-08-07 16:28:31 -0400

    Beginnings of the third linked list example

--- linked_lists/src/third.rs
+++ linked_lists/src/third.rs
@@ -1,3 +1,49 @@
 // Third version - Persistent singly linked stack
-// from https://rust-unofficial.github.io/too-many-lists/second.html
+// from https://rust-unofficial.github.io/too-many-lists/third.html
 
+use::std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    //! Third implementation
+
+    /// Create a new list
+    pub fn new() -> Self {
+        List { 
+            head: None 
+        }
+    }
+
+    pub fn append(&self, elem: T) -> List<T> {
+
+        List { head: Some(
+                        Rc::new(
+                            Node { 
+                                elem: elem,
+                                next: self.head.clone(),
+                                }
+                            )
+                        )
+            }
+
+    }
+    
+    pub fn tail(&self) -> List<T> {
+
+        List { head: self.head.as_ref()
+                         .and_then(|node| node.next.clone()) 
+            }
+
+    }    
+
+}
